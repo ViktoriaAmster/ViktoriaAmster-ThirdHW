2ec5629cecf36cec6a63e5bb1d41908de845f5dd
ViktoriaKukh <bstet@mail.ru>
2022-09-26 21:42:31 +0500

    That's all

---
+++ FourthHW/Program.cs
@@ -0,0 +1,42 @@
+﻿// Console.WriteLine("Hello, World!");
+
+// // Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// Console.WriteLine("Введите основание степени: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите показатель степени: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine($"{a} в {n} степени = {Math.Pow(a,n)}");
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// Console.WriteLine("Введите любое число: ");
+// int Num = Convert.ToInt32(Console.ReadLine());
+// int Sumvoid (int number)
+// {
+//     int result = 0;
+//     while (number > 0)
+//     {
+//         result += number % 10;
+//         number /= 10;
+//     }
+//     return result;
+// }
+
+// Console.WriteLine($"Сумма цифр в числе {Num} = {Sumvoid(Num)}");
+
+
+// // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// String[] arr = new String[8];
+// void LongFerret(String[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = "ferret" + Convert.ToString(i + 1);
+//         Console.Write($"{array[i]} ");
+//     }
+// };
+// LongFerret(arr);
+
